96f71ce89d62ac534ba90e4be39cebaa23e73afd
Scordium <moboman8@gmail.com>
2022-12-12 04:50:30 +0300

    Fix editor crash

--- Source/FirstEquipAnimations/FirstEquipAnimationsModule.cpp
+++ Source/FirstEquipAnimations/FirstEquipAnimationsModule.cpp
@@ -3,10 +3,13 @@
 #include "Patching/NativeHookManager.h"
 
 void FFirstEquipAnimationsModule::StartupModule() {
+
+#if !WITH_EDITOR
 	SUBSCRIBE_METHOD(AFGEquipment::ShouldShowStinger, [](auto& Scope, const AFGEquipment* self)
 	{
 		Scope.Override(true);
 	});
+#endif
 }
 
 IMPLEMENT_GAME_MODULE(FFirstEquipAnimationsModule, FirstEquipAnimations);
